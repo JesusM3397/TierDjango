7bd63fdcbdead741eff44501d06e79b395266e09
JesusM3397 <jesus.mejia.5661@gmail.com>
2023-12-09 19:17:14 -0600

    TITLES

--- provedor/settings.py
+++ provedor/settings.py
@@ -131,3 +131,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SAMESITE = None
